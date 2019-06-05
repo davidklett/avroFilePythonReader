440a0391ec52e563a6c509dd8f730a4406fa49a6
davidklett <david.ger.klett@gmail.com>
2019-06-05 10:26:06 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-# This code was copied from https://avro.apache.org/docs/1.8.2/gettingstartedpython.html
-# and modified for my use case of printing the contents of an avro file.
-import avro.schema
-from avro.datafile import DataFileReader, DataFileWriter
-from avro.io import DatumReader, DatumWriter
-
-schema = avro.schema.parse(open("user.avsc", "rb").read())
-
-#writer = DataFileWriter(open("test.avro", "wb"), DatumWriter(), schema)
-#writer.append({"name": "Alyssa", "favorite_number": 256})
-#writer.append({"name": "Ben", "favorite_number": 7, "favorite_color": "red"})
-#writer.close()
-
-reader = DataFileReader(open("test.avro", "rb"), DatumReader())
-for user in reader:
-    print user
-reader.close()
+from fastavro import block_reader
+with open('test.avro', 'rb') as fo:
+    avro_reader = block_reader(fo)
+    for block in avro_reader:
+        process_block(block)
