29a80c5bff04deeb921fcde73a126931741b5b1b
davidklett <david.ger.klett@gmail.com>
2019-06-05 11:04:21 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-import fastavro
-from fastavro import reader
-with open('test2.avro', 'rb') as fo:
-    avro_reader = reader(fo)
-    for record in avro_reader:
-        process_record(record)
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
+schema = avro.schema.parse(open("test.avsc", "rb").read())
+
+writer = DataFileWriter(open("test2.avro", "wb"), DatumWriter(), schema)
+writer.append({"name": "Alyssa", "favorite_number": 256})
+writer.append({"name": "Ben", "favorite_number": 7, "favorite_color": "red"})
+writer.close()
+
+reader = DataFileReader(open("test2.avro", "rb"), DatumReader())
+for user in reader:
+    print user
+reader.close()
