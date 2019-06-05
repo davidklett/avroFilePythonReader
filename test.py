05576335ad58f57318d8ea83af63123e8495ab6b
davidklett <david.ger.klett@gmail.com>
2019-06-05 10:57:45 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from fastavro import block_reader
-with open('test.avro', 'rb') as fo:
+with open('test2.avro', 'rb') as fo:
     avro_reader = block_reader(fo)
     for block in avro_reader:
         process_block(block)
